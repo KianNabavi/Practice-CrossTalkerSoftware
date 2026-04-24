284bf8b731f5811539ff861d0af2e0d7b70ac751
KianNabavi <50168329+KianNabavi@users.noreply.github.com>
2026-04-24 11:15:05 -0400

    Update cross_talker_2sp_script.js

--- cross_talker_2sp_script.js
+++ cross_talker_2sp_script.js
@@ -102,7 +102,7 @@ function drawRectangles() {
     
     svg.append("image")
         .attr("class", "organelle-image")
-    .attr("x", offsetX + imgWidth/2)
+    .attr("x", offsetX + 126)
     .attr("y", offsetY)
     .attr("width", imgWidth)
     .attr("height", centerSize)
@@ -110,14 +110,14 @@ function drawRectangles() {
     //.attr("preserveAspectRatio", "xMidYMid slice");
 
 
-// svg.append("image")
-//     .attr("class", "organelle-image")
-//     .attr("x", offsetX + centerSize + imgWidth/2)
-//     .attr("y", offsetY)
-//     .attr("width", imgWidth)
-//     .attr("height", centerSize)
-//     .attr("href", "bacteria_picture.png")
-//     //.attr("preserveAspectRatio", "xMidYMid slice");
+svg.append("image")
+    .attr("class", "organelle-image")
+    .attr("x", offsetX + centerSize + 126)
+    .attr("y", offsetY)
+    .attr("width", imgWidth)
+    .attr("height", centerSize)
+    .attr("href", "bacteria_picture.png")
+    //.attr("preserveAspectRatio", "xMidYMid slice");
 
     svg.append("rect")
         .attr("x", offsetX + margin)

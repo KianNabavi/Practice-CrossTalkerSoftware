c5402da80b1b80204fe25c6a8175a851bca1c44e
KianNabavi <50168329+KianNabavi@users.noreply.github.com>
2026-04-24 11:27:37 -0400

    Update cross_talker_2sp_script.js

--- cross_talker_2sp_script.js
+++ cross_talker_2sp_script.js
@@ -102,7 +102,7 @@ function drawRectangles() {
     
     svg.append("image")
         .attr("class", "organelle-image")
-    .attr("x", offsetX + imgWidth/4)
+    .attr("x", offsetX + (centerSize-(imgWidth/2))
     .attr("y", offsetY)
     .attr("width", imgWidth)
     .attr("height", centerSize)
